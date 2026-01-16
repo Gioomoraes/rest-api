9bf64b2021076386327421d2ea8cafe1890033f4
Giovanna Moraes <160680690+Gioomoraes@users.noreply.github.com>
2026-01-16 14:44:23 -0300

    dependências

---
+++ status/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/createproduct', (req, res) => {
+  const { name, price } = req.body
+
+  console.log(name, price)
+
+  res.status(201).json({
+    message: `o produto ${name} foi criado`
+  })
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'primeira rota' })
+})
+
+app.listen(3005, () => {
+  console.log('TEST-POST rodando na porta 3005')
+})
